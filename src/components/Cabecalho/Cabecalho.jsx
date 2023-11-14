32b0e2db8f095f8b3a4def28fd1996ed2deafe39
anapaulans <rm@fiap.com.br>
2023-11-13 22:51:51 -0300

    Fontes do Cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -4,20 +4,20 @@ export default function Cabecalho() {
   return (
     <div>
       <header className="bg-rgb-190-190-202 text-black p-2 flex items-center justify-between h-20">
-        <p className="text-2xl hover:text-blue-500">
+        <p className="text-2xl font-bold hover:text-blue-500">
           <Link href="/">GuinchoSeguro</Link>
         </p>
         <nav className="cabecalhoo flex items-center">
-          <p className="mx-0 text-lg hover:text-blue-500">
+          <p className="mx-0 text-lg font-serif hover:text-blue-500">
             <Link href="/pagesguincho/home">Home</Link>
           </p>
-          <p className="mx-0 text-lg hover:text-blue-500"> 
+          <p className="mx-0 text-lg font-serif hover:text-blue-500"> 
             <Link href="/pagesguincho/chatbot">ChatBot</Link>
           </p>
-          <p className="mx-0 text-lg hover:text-blue-500"> 
+          <p className="mx-0 text-lg font-serif hover:text-blue-500"> 
             <Link href="/pagesguincho/quemsomos">Quem Somos</Link>
           </p>
-          <p className="mx-0 text-lg hover:text-blue-500"> 
+          <p className="mx-0 text-lg font-serif hover:text-blue-500"> 
             <Link href="/pagesguincho/apipy">Funcionários</Link>
           </p>
          
