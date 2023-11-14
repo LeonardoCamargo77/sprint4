f8275486f80f391567a93419205c51bae99a0c89
CalinaThalya <calina.thalya@icloud.com>
2023-11-13 21:45:13 -0300

    Criação da page not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,10 +2,9 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div className="bg-red-400 font-black font-sans m-auto w-96">
-        <h1 className="text-center text-4xl">!OOPS - 404</h1>
-        <p>!Not Found</p>
-  
+    <div className="bg-red-600 font-sans m-auto w-96 h-35 p-4 rounded-lg">
+      <h1 className="text-center text-4xl text-white font-black">ERROR - 404</h1>
+      <p className="text-center text-white">Página não encontrada!</p>
     </div>
-  )
+  );
 }
