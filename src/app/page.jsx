20902fddb3382d243b5f1d6c65bbb629a0ca0729
anapaulans <rm@fiap.com.br>
2023-11-13 23:09:55 -0300

    Alterações finais de botões da page Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="mx-auto max-w-screen-xl">
-      <h1 className="text-4xl font-serif text-center mt-16 mb-24">Tipos de guinchos</h1>
+      <h1 className="text-4xl font-serif text-center mt-16 mb-24">Tipos de Guinchos</h1>
 
       <div className="flex justify-center space-x-8">
         <div className="mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
@@ -33,7 +33,7 @@ export default function Home() {
         
         <Link href="http://localhost:8080/seriesapi/webapi/guincho">
           <div className="border border-blue-500 py-2 px-10 rounded-full text-blue-500 hover:bg-blue-500 hover:text-white cursor-pointer">
-            Saiba mais
+            Ver Detalhes
           </div>
         </Link>
       </div>
