533588ba1f3f6de34fe6de4195454b4122aaac6f
CalinaThalya <calina.thalya@icloud.com>
2023-11-13 22:11:46 -0300

    Criação de botão que retorna a Home

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,25 +1,35 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bg-red-600 font-sans w-96 h-35 p-4 rounded-lg flex-shrink-0">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="bg-red-600 font-sans w-96 h-35 p-4 rounded-lg mb-0">
         <h1 className="text-center text-6xl text-white font-black"> - ERROR - 404</h1>
         <p className="text-center text-white text-3xl">Página não encontrada!</p>
       </div>
-
-      <div className="ml-8 mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300 flex-shrink-0">
+      
+      <div className="mb-0 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
         <figure className="flex flex-col items-center">
           <Image src="/img/killua2.jpg" alt="Killua2" width={400} height={400} />
           <figcaption className="text-center font-thin text-lg"></figcaption>
         </figure>
       </div>
-      </div>
-
       
+      <div className="mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
+        <figure className="flex flex-col items-center">
+          <Image src="/img/killua2.jpg" alt="Killua2" width={400} height={400} />
+          <figcaption className="text-center font-thin text-lg"></figcaption>
+        </figure>
+      </div>
+      <div className="flex justify-end mt-4">
+        
+        <Link href="/">
+          <div className="border border-blue-500 py-2 px-6 rounded-full text-blue-500 hover:bg-blue-500 hover:text-white cursor-pointer">
+            Saiba mais
+          </div>
+        </Link>
+      </div>
+    </div>
     
-
-
-
   );
 }
