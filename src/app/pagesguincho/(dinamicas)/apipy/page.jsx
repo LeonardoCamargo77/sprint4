2e9f744ae26b37870fd45c2210a9295491252a7d
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-11-13 21:00:42 -0300

    Criação botão para API Python

--- src/app/pagesguincho/(dinamicas)/apipy/page.jsx
+++ src/app/pagesguincho/(dinamicas)/apipy/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ApiPython() {
   return (
@@ -6,7 +7,12 @@ export default function ApiPython() {
       <h1 className="text-5xl font-serif text-center mt-16 mb-24">API Python</h1>
 
       <div className="flex justify-center space-x-8">
-        
+      <Link href="http://localhost:8080/seriesapi/webapi/guincho">
+          <div className="border border-blue-500 py-2 px-4 rounded-full text-blue-500 hover:bg-blue-500 hover:text-white cursor-pointer">
+            
+          </div>
+        </Link>
+
       
 
        
