2ce8a4501c8e74b479ec89f7a8fa8020b2efa678
geovanaaprogrammer <rm99646@fiap.com.br>
2023-11-13 23:33:16 -0300

    Adicionando botão Vercel

--- src/app/pagesguincho/(dinamicas)/quemsomos/page.jsx
+++ src/app/pagesguincho/(dinamicas)/quemsomos/page.jsx
@@ -51,6 +51,11 @@ export default function QuemSomos() {
             Repositório
           </div>
       </Link>
+      <Link href="https://github.com/LeonardoCamargo77/sprint4.git">
+          <div className=" text-center border border-blue-500 py-2 px-6 rounded-full text-blue-500 hover:bg-blue-500 hover:text-white cursor-pointer">
+            Vercel
+          </div>
+      </Link>
     </div>
     </div>
 );
