692a393fc7ea444dc8cb4e7f658c1394b73c1a87
geovanaaprogrammer <rm99646@fiap.com.br>
2023-11-13 23:24:37 -0300

    url do repositório + botão

--- src/app/pagesguincho/(dinamicas)/quemsomos/page.jsx
+++ src/app/pagesguincho/(dinamicas)/quemsomos/page.jsx
@@ -45,6 +45,11 @@ export default function QuemSomos() {
 
     <div className="mx-auto max-w-screen-xl">
       <h1 className="text-4xl font-serif text-center mt-16 mb-24">Link do repositório:</h1>
+      <Link href="https://github.com/LeonardoCamargo77/sprint4.git">
+          <div className="border border-blue-500 py-2 px-6 rounded-full text-blue-500 hover:bg-blue-500 hover:text-white cursor-pointer">
+            Repositório
+          </div>
+      </Link>
     </div>
     </div>
 );
