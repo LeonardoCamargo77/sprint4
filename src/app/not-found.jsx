46afb223868230e6e9a6078e0507cfc489d11734
CalinaThalya <calina.thalya@icloud.com>
2023-11-13 22:36:37 -0300

    Ajustes finais da page not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -7,19 +7,12 @@ export default function NotFound() {
       <div className="bg-red-600 font-sans w-96 h-35 p-4 rounded-lg mb-4">
         <h1 className="text-center text-6xl text-white font-black"> - ERROR - 404</h1>
         <p className="text-center text-white text-3xl">Página não encontrada!</p>
-        <div className="mb-0 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
-          <figure className="flex flex-col items-center">
-            <Image src="/img/killua2.jpg" alt="Killua2" width={400} height={400} />
-            <figcaption className="text-center font-thin text-lg"></figcaption>
-          </figure>
-        </div>
-      </div>
-    
-      <div className="mb-4 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
+        <div className="mb-4 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
         <figure className="flex flex-col items-center">
-          <Image src="/img/killua.jpg" alt="Killua" width={400} height={900} />
+          <Image src="/img/killua2.jpg" alt="Killua" width={400} height={400} />
           <figcaption className="text-center font-thin text-lg"></figcaption>
         </figure>
+      </div>
       </div>
 
       <div className="flex justify-end mt-4">
@@ -28,8 +21,18 @@ export default function NotFound() {
           <div className="border border-blue-500 py-2 px-6 rounded-full text-blue-500 hover:bg-blue-500 hover:text-white cursor-pointer">
             HOME
           </div>
-        </Link>
+          </Link>
+        
       </div>
+
+      <div className="mb-4 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
+        <figure className="flex flex-col items-center">
+          <Image src="/img/killua.jpg" alt="Killua" width={400} height={400} />
+          <figcaption className="text-center font-thin text-lg"></figcaption>
+        </figure>
+      </div>
+      
     </div>
+    
   );
 }
