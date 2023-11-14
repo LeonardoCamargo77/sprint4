123d9ec3165b66eeafd1100c9ad5c2d4d6bc856c
anapaulans <rm@fiap.com.br>
2023-11-13 22:44:23 -0300

    Alterações no rodapé

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -4,20 +4,20 @@ export default function Rodape() {
     
 
       <footer className="fixed bottom-0 left-0 w-full bg-rgb-190-190-202 h-20 flex items-center justify-between">
-        <p className="ml-4 text-black font-italic">Todos os direitos reservados</p>
+        <p className="ml-4 text-black font-extralight">Todos os direitos reservados</p>
 
         <div className="flex items-center space-x-4 mr-4">
           <a href="https://github.com/LeonardoCamargo77/sprint4.git" target="_blank" rel="noopener noreferrer" className="text-black">
-            <img src="/img/git.png" alt="GitHub" className="h-9 w-9" />
+            <img src="/img/git.png" alt="GitHub" className="h-10 w-10" />
           </a>
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-black">
-            <img src="/img/face.png" alt="Facebook" className="h-9 w-9" />
+            <img src="/img/face.png" alt="Facebook" className="h-10 w-10" />
           </a>
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-black">
-            <img src="/img/twitter.png" alt="Twitter" className="h-9 w-9" />
+            <img src="/img/twitter.png" alt="Twitter" className="h-10 w-10" />
           </a>
           <a href="https://portoseguro.com.br" target="_blank" rel="noopener noreferrer" className="text-black">
-            <img src="/img/porto3.png" alt="PortoSeguro" className="h-10 w-10" />
+            <img src="/img/porto3.png" alt="PortoSeguro" className="h-11 w-11" />
           </a>
         </div>
       </footer>
