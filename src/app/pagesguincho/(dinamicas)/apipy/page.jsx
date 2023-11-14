7ba4f870210ec12bd791120662feda1cc2587e4e
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-11-13 21:02:45 -0300

    Ajuste na url da api

--- src/app/pagesguincho/(dinamicas)/apipy/page.jsx
+++ src/app/pagesguincho/(dinamicas)/apipy/page.jsx
@@ -7,9 +7,9 @@ export default function ApiPython() {
       <h1 className="text-5xl font-serif text-center mt-16 mb-24">API Python</h1>
 
       <div className="flex justify-center space-x-8">
-      <Link href="http://localhost:8080/seriesapi/webapi/guincho">
+      <Link href="http://127.0.0.1:5000/funcionarios">
           <div className="border border-blue-500 py-2 px-4 rounded-full text-blue-500 hover:bg-blue-500 hover:text-white cursor-pointer">
-            
+            Funcionários
           </div>
         </Link>
 
